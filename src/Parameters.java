9ffa121075bea74fea6697eef1e3e5d546edff83
jac147000@gmail.com <jac147000@gmail.com>
2019-04-21 00:21:43 +0800

    Fix-enable Eclipse project

---
+++ src/Parameters.java
@@ -0,0 +1,16 @@
+
+public class Parameters {
+	
+	public static String img_directory_chosen = "C:\\Users\\user\\source\\eclipse\\UML-EDITOR\\images\\chosen\\";
+	public static String img_directory_unchosen = "C:\\Users\\user\\source\\eclipse\\UML-EDITOR\\images\\unchosen\\";
+	
+	public static String[] file_names_of_buttons= 
+		{
+			"select.png",
+			"association.png",
+		    "general.png",
+		    "composition.png",
+		    "class.png",
+		    "usecase.png"
+		};
+}
